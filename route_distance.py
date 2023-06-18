7c51c8b8da23a6c78de9587cf1d86f2704a3fb29
omartarek198 <omar.tarek29@msa.edu.eg>
2023-06-18 15:18:16 +0300

    added eta calculation

--- route_distance.py
+++ route_distance.py
@@ -1,7 +1,11 @@
 import requests
 
-def calculate_route_distance(origin, destination, api_key):
-    #recieves 1 comma seperated string consisting of lat,long for origin and another one for destination + the api key
+def calculate_route_distance(origin, destination):
+    api_key = "AIzaSyDzF6-vm0EcngSu-_zPXbKUT0MDgsgM-Jw"
+    #input : 2 strings represnting the origin and destination cordinates in the form of 
+    # origin ="longitude,latidue" , source = "longitude,latidue"
+    #output : None if failed to get response else, returns route distance and eta
+    
     url = "https://maps.googleapis.com/maps/api/directions/json"
     params = {
         "origin": origin,
@@ -13,17 +17,19 @@ def calculate_route_distance(origin, destination, api_key):
     data = response.json()
 
     if response.status_code == 200 and data["status"] == "OK":
+        
         route = data["routes"][0]
         distance = route["legs"][0]["distance"]["text"]
-        return distance
+        duration = route["legs"][0]["duration"]["text"]
+        return distance,duration
     else:
         return None
-api_key = "AIzaSyDzF6-vm0EcngSu-_zPXbKUT0MDgsgM-Jw"
+
 
 origin = "40.7128,-74.0060"  
 destination = "34.0522,-118.2437"
 
-distance = calculate_route_distance(origin, destination, api_key)
+distance = calculate_route_distance(origin, destination)
 
 if distance:
     print("Route distance:", distance)
