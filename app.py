da1441bfebf106e37991a0275a87cd9b29475b5b
ahmedemam2 <88343933+ahmedemam2@users.noreply.github.com>
2023-06-18 21:51:03 +0300

    test

--- app.py
+++ app.py
@@ -1,93 +1,49 @@
-import flask
-import flask_login
+import mysql.connector
+from flask import Flask, request, redirect, render_template
 
-app = flask.Flask(__name__)
-app.secret_key = 'kodqwpjdqwodOIWJDnqDIOJ209E019'
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
+app = Flask(__name__)
 
-users = {}
+# Configure MySQL connection
+db = mysql.connector.Connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='system'
+)
 
-class User(flask_login.UserMixin):
-    pass
-
-@login_manager.user_loader
-def user_loader(email):
-    if email not in users:
-        return
-
-    user = User()
-    user.id = email
-    return user
-
-@login_manager.request_loader
-def request_loader(request):
-    email = request.form.get('email')
-    if email not in users:
-        return
-
-    user = User()
-    user.id = email
-    return user
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    if flask.request.method == 'GET':
-        return '''
-               <form action='login' method='POST'>
-                <input type='text' name='email' id='email' placeholder='email'/>
-                <input type='password' name='password' id='password' placeholder='password'/>
-                <input type='submit' name='submit'/>
-               </form>
-               '''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    email = flask.request.form['email']
-    print(f'{users=}')
-    print(f'{email=}')
-    print(f'{flask.request.form["password"]=}')
+        with db.cursor() as cursor:
+            query = "SELECT user_type FROM users WHERE username = %s AND password = %s"
+            cursor.execute(query, (username, password))
+            user = cursor.fetchone()
 
-    if email in users and flask.request.form['password'] == users[email]['password']:
-        user = User()
-        user.id = email
-        flask_login.login_user(user)
-        return flask.redirect(flask.url_for('protected'))
+        if user:
+            user_type = user['user_type']
+            if user_type == 'admin':
+                return redirect('/admin')
+            elif user_type == 'passenger':
+                return redirect('/passenger')
+            elif user_type == 'driver':
+                return redirect('/driver')
 
-    return 'Bad login'
+    return render_template('login_page.html')
 
-@app.route('/protected')
-@flask_login.login_required
-def protected():
-    return 'Logged in as: ' + flask_login.current_user.id
+@app.route('/admin')
+def admin():
+    return render_template('admin_page.html')
 
-@app.route('/logout')
-def logout():
-    flask_login.logout_user()
-    return 'Logged out'
+@app.route('/passenger')
+def passenger():
+    return render_template('passenger_page.html')
 
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return 'Unauthorized', 401
+@app.route('/driver')
+def driver():
+    return render_template('driver_page.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if flask.request.method == 'GET':
-        return """
-        <form action='signup' method='POST'>
-            <input type='text' name='email' id='email' placeholder='email'/>
-            <input type='password' name='password' id='password' placeholder='password'/>
-            <input type='password' name='password' id='password' placeholder='password'/> 
-            <input type='submit' name='submit'/>
-        </form>
-        """
-    
-
-    email = flask.request.form['email']
-    password = flask.request.form['password']
-    users[email] = {'password': password}
-
-    print(f'{users=}')
-    return flask.redirect(flask.url_for('login'))
-
-@app.route('/')
-def index():
-    return flask.redirect(flask.url_for('signup'))
+if __name__ == '__main__':
+    app.run(debug=True)
